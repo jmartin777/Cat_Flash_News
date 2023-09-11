18f37b1cf77c93a2e7f2a2e54ac7140242bf25af
Joshua Martin <ursa8minority@gmail.com>
2023-09-11 17:49:44 -0600

    News Container layout / prop types implemented

---
+++ src/NewsContainer/NewsContainer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Error from '../Error/Error.js';
+import '../App/App.css';
+import '../ArticlesContainer/ArticlesContainer.css';
+
+const renderArticles = (articles) => {
+  return articles.map((article) =>
+    article.title && article.urlToImage && article.description && article.url ? (
+      <div key={article.title} className="article-item">
+        <img src={article.urlToImage} alt={article.title} className="article-image" />
+        <h3 className="article-title">{article.title}</h3>
+        <p className="article-description">{article.description}</p>
+        <a href={article.url} className="article-link" target="_blank" rel="noopener noreferrer">
+          Read More
+        </a>
+      </div>
+    ) : null
+  );
+};
+
+const ArticlesContainer = ({ articles, setError }) => {
+  const isLoading = articles.length === 0;
+
+  return (
+    <div className="articles-container">
+      {isLoading ? (
+        <p></p>
+      ) : articles.length === 0 ? (
+        <p>No articles match criteria.</p>
+      ) : (
+        <div className="articles-list">{renderArticles(articles)}</div>
+      )}
+    </div>
+  );
+};
+
+ArticlesContainer.propTypes = {
+  articles: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      urlToImage: PropTypes.string,
+      description: PropTypes.string,
+      url: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  setError: PropTypes.func.isRequired,
+};
+
+export default ArticlesContainer;
